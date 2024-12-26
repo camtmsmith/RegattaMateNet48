df9ffd3678b05dc847762a8e21e073108b26a41e
camtmsmith <cam.tm.smith@gmail.com>
2024-12-26 15:05:35 +1000

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,70 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class FrmMain
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.btnConnect = New System.Windows.Forms.Button()
+        Me.lblConnect = New System.Windows.Forms.Label()
+        Me.TimerConnect = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.SuspendLayout()
+        '
+        'btnConnect
+        '
+        Me.btnConnect.Location = New System.Drawing.Point(194, 4)
+        Me.btnConnect.Name = "btnConnect"
+        Me.btnConnect.Size = New System.Drawing.Size(75, 23)
+        Me.btnConnect.TabIndex = 0
+        Me.btnConnect.Text = "CONNECT"
+        Me.btnConnect.UseVisualStyleBackColor = True
+        '
+        'lblConnect
+        '
+        Me.lblConnect.AutoSize = True
+        Me.lblConnect.Location = New System.Drawing.Point(12, 9)
+        Me.lblConnect.Name = "lblConnect"
+        Me.lblConnect.Size = New System.Drawing.Size(112, 13)
+        Me.lblConnect.TabIndex = 1
+        Me.lblConnect.Text = "Control not connected"
+        '
+        'FrmMain
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.lblConnect)
+        Me.Controls.Add(Me.btnConnect)
+        Me.Name = "FrmMain"
+        Me.Text = "Regatta Mate - Timing System"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SerialPort1 As IO.Ports.SerialPort
+    Friend WithEvents btnConnect As Button
+    Friend WithEvents lblConnect As Label
+    Friend WithEvents TimerConnect As Timer
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,97 @@
+﻿' Regatta Mate
+' Arduino Nano and Visual Basic
+' Capture of finish margins, storing and editing.
+
+Imports System.IO
+Imports System.IO.Ports
+
+Public Class FrmMain
+    ' Global variables.
+    ' Anything defined here is available in the whole app
+    Dim ArduinoConnected As Boolean
+
+    ' This called when the app first starts. Used to initial what ever needs initialising.
+    Private Sub FrmMain_Load(sender As Object, e As EventArgs) Handles Me.Load
+        btnConnect.BackColor = Color.IndianRed
+        TimerConnect.Enabled = False
+        ArduinoConnected = False
+        autoconnect() 'Search for Finish control
+        'Me.Show()
+    End Sub
+
+    ' Try to open the com port or close the port if already open
+    ' Note: There is no error catching. If the connection attampt fails the app will crash!
+    Private Sub btnConnect_Click(sender As Object, e As EventArgs) Handles btnConnect.Click
+        If (btnConnect.Text = "CONNECT") Then
+            autoconnect()
+        Else
+            Try
+                SerialPort1.Close()
+                btnConnect.Text = "CONNECT"
+                ArduinoConnected = False
+            Catch ex As Exception
+                MessageBox.Show("Serial Port is already closed!")
+            End Try
+        End If
+    End Sub
+    'Seach open serial port for connection, send "3/" and listen for reply
+    Private Sub autoconnect()
+        ' Dafault Port parameters
+        SerialPort1.BaudRate = 9600                         '**
+        SerialPort1.DataBits = 8                            '**
+        SerialPort1.Parity = Parity.None                    '**
+        SerialPort1.StopBits = StopBits.One                 '**
+        SerialPort1.Handshake = Handshake.None              '**
+        SerialPort1.Encoding = System.Text.Encoding.Default '**
+
+        SerialPort1.DtrEnable = True
+        SerialPort1.RtsEnable = True
+        SerialPort1.ReadTimeout = 10000
+
+        For Each sp As String In My.Computer.Ports.SerialPortNames
+            Try
+                ' Try each port found
+                SerialPort1.PortName = sp
+                SerialPort1.Open()
+                'Sent msg and wait for response
+                SerialPort1.Write("3")
+                TimerConnect.Interval = 500
+                TimerConnect.Start()
+                Dim str As String = SerialPort1.ReadExisting()
+                If str.Contains("51") Then
+                    'MsgBox("Control Found " + SerialPort1.PortName + " - " + str)
+                    ArduinoConnected = True
+                    btnConnect.Text = "Connected !" + SerialPort1.PortName
+                End If
+                While TimerConnect.Enabled And Not (ArduinoConnected)
+                    Application.DoEvents()
+                End While
+                If ArduinoConnected Then
+                    Exit For
+                End If
+                SerialPort1.Close()
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+        Next
+        ' Format Label and Button depending on connection status
+        If ArduinoConnected Then
+            lblConnect.Text = "Connectet on: " + SerialPort1.PortName
+            btnConnect.Text = "DIS-CONNECT"
+            btnConnect.BackColor = SystemColors.Control
+            'TimerConnect.Enabled = True
+            btnConnect.Text = "Disconnect" '' old button
+        Else
+            lblConnect.Text = "Control Not connected"
+            btnConnect.Text = "CONNECT"
+            btnConnect.BackColor = Color.IndianRed
+            Timer1.Enabled = False
+            MsgBox("Timing control failed to connect. Please check that it is plugged in.")
+        End If
+    End Sub
+
+    ' Timer for arduino search
+    Public Sub TimerConnect_Tick(sender As Object, e As EventArgs) Handles TimerConnect.Tick
+        TimerConnect.Stop()
+    End Sub
+End Class
